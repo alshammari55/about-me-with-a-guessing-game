f3af712b5f737a8e80a7c3c9efd5726a34e30677
alshammari55 <alialshammari663@yahoo.com>
2021-07-26 20:39:51 +0300

    lab02

---
+++ js/app.js
@@ -0,0 +1,82 @@
+'use strict';
+
+let q = prompt("whats your name ?") ;
+alert("welcome mr "+ q ) ;
+
+
+
+let q1 = prompt("are you from jordan ?") ;
+switch(q1) {
+    case 'yes':
+alert('you are welcome') ;
+//console.log(q1)
+      break;
+    case 'no':
+alert('you are welcome') ;
+      break;
+    default:
+alert('you didnt choose') ;
+  }
+
+
+
+let q2 = prompt("do you like the it fields ?") ;
+
+switch(q2) {
+    case 'yes':
+alert('thats cool') ;
+//console.log(q2)
+      break;
+    case 'no':
+alert('you are wrong') ;
+
+      break;
+    default:
+alert('you must answer') ;
+
+  }
+
+let q3 = prompt("do you like my webpage?") ;
+
+
+switch(q3) {
+    case 'yes':
+alert('thats kind from you') ;
+//console.log(q3)
+
+      break;
+    case 'no':
+alert('its your opinion') ;
+      break;
+    default:
+alert('you must answer') ;
+
+  }
+let q4 = prompt("is this your first time here ?") ;
+
+
+switch(q4) {
+    case 'yes':
+alert('you are welcome') ;
+//console.log(q4)      
+      break;
+    case 'no':
+alert('we miss you') ;
+      break;
+    default:
+alert('come back') ;
+  }
+let q5 = prompt("would you like to visit us again?") ;
+switch(q5) {
+    case 'yes':
+alert('we will wait') ;
+//console.log(q5)
+      break;
+    case 'no':
+alert('its up to you') ;
+
+      break;
+    default :
+alert('you have to answer') ;
+
+  }
