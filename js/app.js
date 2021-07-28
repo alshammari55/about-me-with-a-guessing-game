904786dcb08d1e8788a716056fccca8e1e8e6cd7
alshammari55 <alialshammari663@yahoo.com>
2021-07-28 17:51:33 +0300

    function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 'use strict';
-
 var correct = 0
+question1();
+question2();
+question3();
+question4();
+question5();
+question6();
+question7();
 
+function question1 () {
 let q = prompt("whats your name ?") ;
 alert("welcome mr "+ q ) ;
-
-
-
 let q1 = prompt("are you from jordan ?") ;
 switch(q1) {
     case 'yes':
@@ -20,11 +24,10 @@ alert('you are welcome') ;
     default:
 alert('you didnt choose') ;
   }
+}
 
-
-
+function question2 (){
 let q2 = prompt("do you like the it fields ?") ;
-
 switch(q2) {
     case 'yes':
 alert('thats cool') ;
@@ -39,10 +42,10 @@ alert('you are wrong') ;
 alert('you must answer') ;
 
   }
+}
 
+function question3 (){
 let q3 = prompt("do you like my webpage?") ;
-
-
 switch(q3) {
     case 'yes':
 alert('thats kind from you') ;
@@ -57,9 +60,10 @@ alert('its your opinion') ;
 alert('you must answer') ;
 
   }
+}
+
+function question4 (){
 let q4 = prompt("is this your first time here ?") ;
-
-
 switch(q4) {
     case 'yes':
 alert('you are welcome') ;
@@ -72,6 +76,9 @@ alert('we miss you') ;
     default:
 alert('come back') ;
   }
+}
+
+function question5 (){
 let q5 = prompt("would you like to visit us again?") ;
 switch(q5) {
     case 'yes':
@@ -87,8 +94,9 @@ alert('its up to you') ;
 alert('you have to answer') ;
 
   }
+}
 
-  
+  function question6(){
   let guess = prompt('Guess a number between 1 and 10');
   let numGuess = Number(guess);
   let answer = 7;
@@ -104,11 +112,10 @@ alert('you have to answer') ;
       numGuess = parseInt(prompt('That is too high, guess again!'));
   }
   alert('The correct answer was 7!');
-  
+}
 
-
+function question7 (){
 let favcar = ['bmw', 'nissan', 'mercedes', 'volkswagen', 'kia'];
-
   for(let x = 0; x < 6; x++ ){
     let bigGuess = prompt('Can you guess one of my favorite cars?');
     if (favcar.includes(bigGuess)){
@@ -121,3 +128,4 @@ let favcar = ['bmw', 'nissan', 'mercedes', 'volkswagen', 'kia'];
   }
 
   alert('You got' + correct + '/7');
+}
